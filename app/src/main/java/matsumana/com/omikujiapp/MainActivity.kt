565396b135dfbb8aaa567cb9addacc7ac5ae1bf4
matsumana07384 <manami.m.227@outlook.jp>
2018-06-10 22:22:21 +0900

    画像ファイル追加/列挙体を使用

--- app/src/main/java/matsumana/com/omikujiapp/MainActivity.kt
+++ app/src/main/java/matsumana/com/omikujiapp/MainActivity.kt
@@ -19,33 +19,69 @@ class MainActivity : AppCompatActivity() {
 
 //        val button = findViewById(R.id.button_id)
         getOmikujiButton.setOnClickListener {
+
             // 画面上に文字を出力
 //            Log.v("MainActivity","Button Clicked")
-            val results = arrayOf("大吉","吉","中吉","小吉","末吉","凶","大凶")
 
+//            val results = arrayOf("大吉","吉","中吉","小吉","末吉","凶","大凶")
 //            val n = Random().nextInt(10 )
-            val n = Random().nextInt(results.count())
+//            val n = Random().nextInt(results.count())
 
+            val results = OmikujiResults.values()
+
+            val n = Random().nextInt(OmikujiResults.values().count())
             val omikujiImage = findViewById<View>(R.id.resultOmikuji) as ImageView
 
+            //インスタンス化
+//            outPutOmikuji(omikuji = results[n])
 
-            if (results[n] === "大吉" ){
-//                resultTextView.setTextColor(Color.RED)
-//                resultTextView.setTextColor(Color.parseColor("#ff0000"))
-//                ↑でも同じ色を出すことができる
-                resultTextView.setTextColor(Color.argb(255,255,0,0))
-                //おみくじを変える
-                omikujiImage.setImageResource(R.drawable.omikuji_daikichi)
 
-            }else{
-                resultTextView.setTextColor(Color.parseColor("#808080"))
-                omikujiImage.setImageResource(R.drawable.omikuji)
+            when(results[n]){
+
+                OmikujiResults.大吉 -> omikujiImage.setImageResource(R.drawable.omikuji_daikichi)
+
+                OmikujiResults.吉 -> omikujiImage.setImageResource(R.drawable.omikuji_kichi)
+
+                OmikujiResults.中吉 -> omikujiImage.setImageResource(R.drawable.omikuji_chuukichi)
+
+                OmikujiResults.小吉 -> omikujiImage.setImageResource(R.drawable.omikuji_syoukichi)
+
+                OmikujiResults.末吉 -> omikujiImage.setImageResource(R.drawable.omikuji_suekichi)
+
+                OmikujiResults.凶 -> omikujiImage.setImageResource(R.drawable.omikuji_kyou)
+
+                OmikujiResults.大凶 -> omikujiImage.setImageResource(R.drawable.omikuji_daikyou)
+
+
             }
 
+//            resultTextView.text = results[n].toString()
+
+
+//            if (results[n] === OmikujiResults.大吉 ){
+////                resultTextView.setTextColor(Color.RED)
+////                resultTextView.setTextColor(Color.parseColor("#ff0000"))
+////                ↑でも同じ色を出すことができる
+//                resultTextView.setTextColor(Color.argb(255,255,0,0))
+//                //おみくじを変える
+//                omikujiImage.setImageResource(R.drawable.omikuji_daikichi)
+//
+//            }else if (results[n] === OmikujiResults.中吉 ){
+//                //おみくじを変える
+//                omikujiImage.setImageResource(R.drawable.omikuji_chuukichi)
+//
+//            }
+//            else{
+//                resultTextView.setTextColor(Color.parseColor("#808080"))
+//                omikujiImage.setImageResource(R.drawable.omikuji)
+//
+//            }
+
 //            resultTextView.text = n.toString()
-            resultTextView.text = results.get(n)
+//            resultTextView.text = results.get(n)
+            resultTextView.text = results.get(n).toString()
         }
     }
-}
+  }
 
 
---
+++ app/src/main/java/matsumana/com/omikujiapp/OmikujiResults.kt
@@ -0,0 +1,16 @@
+package matsumana.com.omikujiapp
+
+//enum class OmikujiResults(val omikuji: Int){
+
+enum class OmikujiResults(val comments: String){
+    大吉("やったね！"),
+    吉("いい感じ！"),
+    中吉("よいですね"),
+    小吉("ちいさな幸せ見つけていこう"),
+    末吉("そういう日もある"),
+    凶("どんまい"),
+    大凶("明日があるさ")
+}
+
+
+
